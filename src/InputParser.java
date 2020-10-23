fce70d55aa3deaac6fa7208b9825eb640b8bce4c
Kashish708 <s.kashish0123@gmail.com>
2020-10-23 15:34:17 -0400

    Created the skeleton and wrote the getCommand() and getInt() methods

--- src/InputParser.java
+++ src/InputParser.java
@@ -1,4 +1,47 @@
+import java.util.Scanner;
+
 public class InputParser {
 
+    private Scanner reader;
 
+    public InputParser() {
+        reader = new Scanner(System.in);
+    }
+
+    public CommandWord getCommand() {
+        String inputLine;
+        String word = null;
+
+        System.out.print("> ");     // print prompt
+
+        inputLine = reader.nextLine();
+
+        // Finding the word on the line.
+        Scanner tokenizer = new Scanner(inputLine);
+        if (tokenizer.hasNext()) {
+            word = tokenizer.next();      // get word
+
+        }
+        return CommandWord.valueOf(word);
+    }
+
+    /*public Territory getTerritory() {
+
+    }*/
+
+    public int getInt() {
+
+        String inputLine;
+
+        inputLine = reader.nextLine();
+
+        char[] chars = inputLine.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char c : chars) {
+            if (Character.isDigit(c)) {
+                sb.append(c);
+            }
+        }
+        return Integer.parseInt(sb.toString());
+    }
 }
