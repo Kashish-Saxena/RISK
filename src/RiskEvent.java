67334267240a6971fc2bbc1f192f9b375dddcc30
kevinhquach <>
2020-11-08 23:48:11 -0500

    idk why these didn't get included

--- src/RiskEvent.java
+++ src/RiskEvent.java
@@ -1,25 +1,26 @@
 import java.util.EventObject;
+import java.util.List;
 
 public class RiskEvent extends EventObject {
-    private Territory territoryFrom;
-    private Territory territoryTo;
+    private TurnPhase phase;
+    private Player currentPlayer;
     /**
      * Constructs a prototypical Event.
      *
      * @param rg The object on which the Event initially occurred.
      * @throws IllegalArgumentException if source is null.
      */
-    public RiskEvent(RiskGame rg, Territory territoryFrom, Territory territoryTo) {
+    public RiskEvent(RiskGame rg, TurnPhase phase, Player currentPlayer) {
         super(rg);
-        this.territoryFrom = territoryFrom;
-        this.territoryTo = territoryTo;
+        this.phase = phase;
+        this.currentPlayer = currentPlayer;
     }
 
-    public Territory getTerritoryFrom() {
-        return territoryFrom;
+    public TurnPhase getPhase() {
+        return phase;
     }
 
-    public Territory getTerritoryTo() {
-        return territoryTo;
+    public Player getCurrentPlayer() {
+        return currentPlayer;
     }
 }
--- src/RiskFrame.java
+++ src/RiskFrame.java
@@ -10,6 +10,8 @@ public class RiskFrame extends JFrame implements RiskView {
     private RiskMap riskMap;
     private RiskGame rg;
     private RiskMapPanel mapPanel;
+    private JLabel turn;
+    private JLabel info;
 
     private ArrayList<Shape> territoryCircles; // Create an ArrayList object
 
@@ -34,13 +36,16 @@ public class RiskFrame extends JFrame implements RiskView {
 
         JPanel playerInputPanel = new JPanel();
         JPanel turnpanel = new JPanel();
-        JLabel turn = new JLabel("Player's turn");
+        turn = new JLabel("Player's turn");
+        info = new JLabel("Choose a territory to attack from.");
         turnpanel.add(turn);
         turnpanel.add(info);
         JPanel buttonpanel = new JPanel();
         //JButton attack = new JButton("ATTACK");
         JButton pass = new JButton("PASS");
-        buttonpanel.add(attack);
+        pass.setActionCommand("pass");
+
+        //buttonpanel.add(attack);
         buttonpanel.add(pass);
         playerInputPanel.add(turnpanel);
         playerInputPanel.add(buttonpanel);
@@ -65,6 +70,100 @@ public class RiskFrame extends JFrame implements RiskView {
     @Override
     public void handleRiskUpdate(RiskEvent e) {
         mapPanel.handleRiskUpdate(e);
+        turn.setText(e.getCurrentPlayer().getName() + "'s turn"); //include the phase
+
+
+
+        if (e.getPhase() == TurnPhase.ATTACK_CHOOSE_DICE) { //update this to use new phases
+            //info.setText("Choose a number of dice to attack with."); //list options
+            //TODO: wrap JOptionPane in the try catch, or implement a spinner
+            String str = JOptionPane.showInputDialog("Choose a number of dice to attack with (1 - " + ((RiskEventBounds)e).getMaxChoice() + ")"); //list options
+            int diceNum = 0;
+            try
+            {
+                if(str != null)
+                    diceNum = Integer.parseInt(str);
+            }
+            catch (NumberFormatException excp)
+            {
+                diceNum = 0;
+            }
+            while (((diceNum > ((RiskEventBounds)e).getMaxChoice()) || (diceNum < ((RiskEventBounds)e).getMinChoice()))) {
+                System.out.println(diceNum);
+                System.out.println((!(diceNum > ((RiskEventBounds)e).getMaxChoice()) || !(diceNum < ((RiskEventBounds)e).getMinChoice())));
+                diceNum = Integer.parseInt(JOptionPane.showInputDialog("Invalid. Please enter number of dice between (1 - " + ((RiskEventBounds)e).getMaxChoice() + ")"));
+            }
+            rg.setAttackDice(diceNum); //note! some controller logic in here
+        }
+        else if (e.getPhase() == TurnPhase.DEFEND_CHOOSE_DICE) {
+            //TODO: wrap JOptionPane in the try catch, or implement a spinner
+            String str = JOptionPane.showInputDialog("Choose a number of dice to defend with (1 - " + ((RiskEventBounds)e).getMaxChoice() + ")"); //list options
+            int diceNum = 0;
+            try
+            {
+                if(str != null)
+                    diceNum = Integer.parseInt(str);
+            }
+            catch (NumberFormatException excp)
+            {
+                diceNum = 0;
+            }
+            while (((diceNum > ((RiskEventBounds)e).getMaxChoice()) || (diceNum < ((RiskEventBounds)e).getMinChoice()))) {
+                System.out.println(diceNum);
+                System.out.println((!(diceNum > ((RiskEventBounds)e).getMaxChoice()) || !(diceNum < ((RiskEventBounds)e).getMinChoice())));
+                diceNum = Integer.parseInt(JOptionPane.showInputDialog("Invalid. Please enter number of dice between (1 - " + ((RiskEventBounds)e).getMaxChoice() + ")"));
+            }
+            rg.setDefendDice(diceNum); //note! some controller logic in here
+        }
+        else if (e.getPhase() == TurnPhase.ATTACK_CHOOSE_MOVE) {
+            //TODO: wrap JOptionPane in the try catch, or implement a spinner
+            String str = JOptionPane.showInputDialog("Choose a number of dice to defend with (1 - " + ((RiskEventBounds)e).getMaxChoice() + ")"); //list options
+            int diceNum = 0;
+            try
+            {
+                if(str != null)
+                    diceNum = Integer.parseInt(str);
+            }
+            catch (NumberFormatException excp)
+            {
+                diceNum = 0;
+            }
+            while (((diceNum > ((RiskEventBounds)e).getMaxChoice()) || (diceNum < ((RiskEventBounds)e).getMinChoice()))) {
+                System.out.println(diceNum);
+                System.out.println((!(diceNum > ((RiskEventBounds)e).getMaxChoice()) || !(diceNum < ((RiskEventBounds)e).getMinChoice())));
+                diceNum = Integer.parseInt(JOptionPane.showInputDialog("Invalid. Please enter number of armies between (" + ((RiskEventBounds)e).getMinChoice() + " - " + ((RiskEventBounds)e).getMaxChoice() + ")"));
+            }
+            rg.move(diceNum); //note! some controller logic in here
+        }
+        else if (e.getPhase() == TurnPhase.ATTACK_RESULT) {
+            RiskEventDiceResults diceResults = (RiskEventDiceResults)e;
+
+            String message = "";
+            for (Integer i : diceResults.getAttackDice()) {
+                message += i + ", ";
+            }
+            message = "Attacker rolls: " + message.substring(0, message.length() - 2) + "\n";
+
+            String diceRolls = "";
+            for (Integer i : diceResults.getDefendDice()) {
+                diceRolls += i + ", ";
+            }
+            diceRolls = diceRolls.substring(0, diceRolls.length() - 2);
+            message += "Defender rolls: " + diceRolls + "\n"
+                + "Attackers (" + diceResults.getTerritoryFrom().getName() + ") lost " + diceResults.getAttackLoss() + " armies.\n";
+
+            if (diceResults.getDefendLoss() != -1) {
+                message += "Defenders (" + diceResults.getTerritoryTo().getName() + ") lost " + diceResults.getDefendLoss() + " armies.";
+            }
+            else {
+                message += diceResults.getCurrentPlayer().getName() + " took over " + diceResults.getDefender().getName() + "'s " + diceResults.getTerritoryTo().getName();
+            }
+            JOptionPane.showMessageDialog(this, message);
+        }
+        else if (e.getPhase() == TurnPhase.END) {
+            JOptionPane.showMessageDialog(this, "game over man");
+        }
+
     }
 
     //main
