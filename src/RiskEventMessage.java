53e597676205420bf89c19dd9fb6a6ee7354cf5e
Kashish708 <s.kashish0123@gmail.com>
2020-11-22 20:30:34 -0500

    Added Javadoc Comments

--- src/RiskEventMessage.java
+++ src/RiskEventMessage.java
@@ -1,3 +1,9 @@
+/**
+ * The RiskEventMessage class extends the RiskEvent class and gets the updated message to display.
+ *
+ * @author David Sciola - 101082459 and Kevin Quach - 101115704
+ * @version November 23, 2020
+ */
 
 public class RiskEventMessage extends RiskEvent{
 
@@ -7,11 +13,22 @@ public class RiskEventMessage extends RiskEvent{
 
     private String message;
 
+    /**
+     * Constructor of the RiskEventMessage class. It initializes all the field values.
+     * @param rg The RiskGame class.
+     * @param phase The TurnPhase of the player.
+     * @param currentPlayer The current player.
+     * @param message The updated message.
+     */
     public RiskEventMessage(RiskGame rg, TurnPhase phase, Player currentPlayer, String message) {
         super(rg, phase, currentPlayer);
         this.message = message;
     }
 
+    /**
+     * Returns the message.
+     * @return The message string.
+     */
     public String getMessage(){
         return this.message;
     }
--- src/RiskEventSingleTerritory.java
+++ src/RiskEventSingleTerritory.java
@@ -1,14 +1,30 @@
-
+/**
+ * The RiskEventSingleTerritory class extends the RiskEvent class and gets the updated territory.
+ *
+ * @author David Sciola - 101082459 and Kevin Quach - 101115704
+ * @version November 23, 2020
+ */
 
 public class RiskEventSingleTerritory extends RiskEvent{
 
     private Territory territory;
 
+    /**
+     * Constructor of the RiskEventSingleTerritory class. It initializes all the field values.
+     * @param rg The RiskGame class.
+     * @param phase The TurnPhase of the player.
+     * @param currentPlayer The current player.
+     * @param territory A territory.
+     */
     public RiskEventSingleTerritory(RiskGame rg, TurnPhase phase, Player currentPlayer, Territory territory) {
         super(rg, phase, currentPlayer);
         this.territory = territory;
     }
 
+    /**
+     * Returns the territory object.
+     * @return The territory object.
+     */
     public Territory getTerritory() {
         return territory;
     }
